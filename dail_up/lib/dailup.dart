09386ccb3e8125d0eb44c594c3fbc80d7674014e
mariam-1997 <Maarouf.mariam97@gmail.com>
2020-05-28 11:27:51 +0000

    first comit

---
+++ dail_up/lib/dailup.dart
@@ -0,0 +1,53 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dialpad/flutter_dialpad.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'main.dart';
+
+
+//dial pad package Documentation here
+//https://pub.dev/packages/flutter_dialpad#-readme-tab-
+class Dailup extends StatefulWidget {
+  final String phone;
+  final String name;
+  Dailup({this.phone,this.name});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<Dailup> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Text("Logout"),
+          backgroundColor: Colors.orange,
+          onPressed: () async {
+            SharedPreferences prefs = await SharedPreferences.getInstance();
+            prefs.remove('phoneno');
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (BuildContext ctx) => Dail_up()));
+          }),
+        backgroundColor: Theme.of(context).accentColor,
+        body: Container(
+          margin: EdgeInsets.only(top: 50),
+                child: SafeArea(
+            child:
+            DialPad(
+                enableDtmf: true,
+                outputMask: "(000) 000-0000",
+                backspaceButtonIconColor: Colors.red,
+                makeCall: (number){
+                  print(number);
+                }
+            )
+        ),
+      )),
+    );
+  }
+}
---
+++ dail_up/lib/verification_screen.dart
@@ -0,0 +1,59 @@
+import 'package:dail_up/dailup.dart';
+import 'package:flutter/material.dart';
+
+class Verification extends StatelessWidget {
+  @override Widget build(BuildContext context) {
+
+    return Scaffold(
+      body:  Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+        Container(
+        width: 300,
+        margin: EdgeInsets.only(top: 150),
+    child: TextField(
+    textAlign: TextAlign.left,
+    keyboardType: TextInputType.number,
+    decoration: InputDecoration(
+    prefixIcon: Icon(Icons.lock),
+    hintText: 'Verification Code',
+    hintStyle: TextStyle(fontSize: 16),
+    border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(23),
+    borderSide: BorderSide(
+    width: 0,
+    style: BorderStyle.none,
+    ),
+    ),
+    filled: true,
+    contentPadding: EdgeInsets.all(16),
+    fillColor: Colors.white,
+    ),
+
+    )),
+    Container(
+    width: 180,
+    margin: const EdgeInsets.only(top: 20.0),
+    child: FlatButton(
+    splashColor: Theme.of(context).primaryColor,
+    padding: EdgeInsets.only(top:15, bottom: 15,),
+    onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => Dailup()));},
+
+    child:
+    Text(
+    "Register",
+    style: TextStyle(color: Theme.of(context).accentColor, fontSize: 18, fontFamily: 'Roboto'),
+    ),
+    shape: RoundedRectangleBorder(
+    borderRadius: new BorderRadius.circular(23.0),
+    ),
+    color: Colors.white,
+    ),
+
+
+    ),
+      ],)
+    );
+  }
+}

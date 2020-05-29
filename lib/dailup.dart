48e1426125bfd27c978f40d3e89f1ae648959f36
Omar Yehia <omaryahiatawfiek@gmail.com>
2020-05-29 18:28:00 +0200

    Arranging path

--- lib/dailup.dart
+++ lib/dailup.dart
@@ -5,20 +5,18 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import 'main.dart';
 
-
 //dial pad package Documentation here
 //https://pub.dev/packages/flutter_dialpad#-readme-tab-
 class Dailup extends StatefulWidget {
   final String phone;
   final String name;
-  Dailup({this.phone,this.name});
+  Dailup({this.phone, this.name});
 
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<Dailup> {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,24 +28,29 @@ class _MyAppState extends State<Dailup> {
           onPressed: () async {
             SharedPreferences prefs = await SharedPreferences.getInstance();
             prefs.remove('phoneno');
-            Navigator.pushReplacement(context,
-                MaterialPageRoute(builder: (BuildContext ctx) => Dail_up()));
-          }),
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (BuildContext ctx) => Dail_up(),
+              ),
+            );
+          },
+        ),
         backgroundColor: Theme.of(context).accentColor,
         body: Container(
           margin: EdgeInsets.only(top: 50),
-                child: SafeArea(
-            child:
-            DialPad(
-                enableDtmf: true,
-                outputMask: "(000) 000-0000",
-                backspaceButtonIconColor: Colors.red,
-                makeCall: (number){
-                  print(number);
-                }
-            )
+          child: SafeArea(
+            child: DialPad(
+              enableDtmf: true,
+              outputMask: "(000) 000-0000",
+              backspaceButtonIconColor: Colors.red,
+              makeCall: (number) {
+                print(number);
+              },
+            ),
+          ),
         ),
-      )),
+      ),
     );
   }
 }
